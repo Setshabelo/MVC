dd2c1843d29293c44e5a00aece290cfd8e4176de
Setshabelo <setshabelo@outlook.com>
2018-03-08 21:24:34 +0200

    Add files via upload

---
+++ RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace MVC.Controllers
+{
+    public class RegisterController : Controller
+    {
+        AllContext context = new AllContext();
+        // GET: Register
+        public ActionResult Registering()
+        {
+          return View();
+        }
+           
+
+        [HttpPost]
+        public ActionResult Registering(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(register).State = System.Data.Entity.EntityState.Added;
+                context.SaveChangesAsync();
+                RedirectToAction("Index", "Home");
+
+            }
+            else
+            {
+                return View("Registering",register);
+            }
+            return RedirectToAction("Login","Login");
+        }
+    }
+}
